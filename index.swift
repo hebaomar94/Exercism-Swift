0f68c95383d3ca98676f28e8e98511566f895e1c
Heba Omar <97067717+hebaomar94@users.noreply.github.com>
2023-07-29 10:12:04 +0300

    Update index.swift

--- index.swift
+++ index.swift
@@ -1,5 +1,7 @@
 //Iteration 1
 
-func hello(_ name: String = "World") -> String {
+func hello(_ name: String = " ") -> String {
     return "Hello, \(name)!"
 }
+let greeting = hello("heba")
+print (greeting)
